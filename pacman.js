9deb2359ecbf641d74ef9706ca624ca5f8fa3872
Martin Egert <m.egert82@gmail.com>
2022-12-12 13:22:34 +0100

    only trial and error with no outcome done

---
+++ pacman.js
@@ -0,0 +1,13 @@
+const SPRITE_SIZE = 20;
+
+const pacman = document.querySelector("#pacman");
+pacman.style.position = "absolute";
+pacman.style.width = "20px";
+pacman.style.height = `${SPRITE_SIZE}px`;
+
+let pmX = 0;
+let pmY = 0;
+
+// console.log(pacmanSprite);
+
+
