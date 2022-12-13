0688ffa03943f7b55b2d7e60005584171c7a68e4
zinaida <zinaida.j84@gmail.com>
2022-12-13 11:14:30 +0100

    class Node added set and get, added testNodes variable

--- src/nodes.js
+++ src/nodes.js
@@ -1,8 +1,112 @@
 class Node {
   constructor(x, y) {
+    this.x = x;
+    this.y = y;
     this.position = [x, y];
     this.neighbors = { UP: null, DOWN: null, LEFT: null, RIGHT: null };
   }
+  get getX() {
+    return this.x;
+  }
+  get getY() {
+    return this.y;
+  }
+  set right(newRight) {
+    this.neighbors.RIGHT = newRight;
+  }
+  set left(newLeft) {
+    this.neighbors.LEFT = newLeft;
+  }
+  set up(newUp) {
+    this.neighbors.UP = newUp;
+  }
+  set down(newDown) {
+    this.neighbors.DOWN = newDown;
+  }
+}
+//8X8
+const nodeSymbols = ["X", "+", "x"];
+const pathSymbols = ["."];
+const mazeTest = {
+  sizeX: 8,
+  sizeY: 8,
+  pattern:
+    "########" +
+    "#X..X###" +
+    "#.##.###" +
+    "#X..X.X#" +
+    "#.####.#" +
+    "#.####.#" +
+    "#X....X#" +
+    "########",
+};
+/**
+ * Reads the position in string, and converts in actual coordinate
+ * @returns
+ */
+function constructKey(x, y, dx = RASTER_SIZE, dy = RASTER_SIZE) {
+  return { x: x * dx, y: y * dy };
+}
+/**
+ * Reads level information and creates list of unconnected Nodes
+ * @param {Object} level
+ * @returns {[Node]}
+ */
+function createNodeTable(level) {
+  let nodeList = [];
+  let count = 0;
+  for (let y = 0; y < level.sizeY; y++) {
+    for (let x = 0; x < level.sizeX; x++) {
+      if (nodeSymbols.includes(level.pattern[count])) {
+        let coord = constructKey(x, y);
+        let newNode = new Node(coord.x, coord.y);
+        nodeList.push(newNode);
+      }
+      count += 1;
+    }
+  }
+  return nodeList;
+}
+
+// TEST
+let nodes = createNodeTable(mazeTest);
+// console.log(nodes);
+
+function getNodeByCoord(x, y, nodes) {
+  for (const node of nodes) {
+    console.log(node.getX);
+    if (node.getX === x && node.getY === y) return node;
+  }
+  return null;
+}
+// TEST
+// console.log(getNodeByCoord(100, 25, nodes));
+
+function connectHorizontally(level, nodeList) {
+  let count = 0;
+  for (let y = 0; y < level.sizeY; y++) {
+    let key = null;
+    for (let x = 0; x < level.sizeX; x++) {
+      if (nodeSymbols.includes(level.pattern[count])) {
+        if (key === null) {
+          key = constructKey(x, y);
+        } else {
+          let otherkey = constructKey(x, y);
+          let firstNode = getNodeByCoord(key.x, key.y);
+          let secondNode = getNodeByCoord(otherkey.x, otherkey.y);
+          firstNode.right = secondNode;
+          secondNode.left = firstNode;
+        }
+      }
+      count += 1;
+    }
+  }
+  return;
+}
+//
+
+function connectVertically() {
+  return;
 }
 
 function testNodes() {
@@ -37,4 +141,6 @@ function renderNodes(nodesList) {
     createTableEl(node.position[0], node.position[1], PALLET_POW_SOURCE);
   }
 }
-renderNodes(testNodes());
+
+let testNodes = testNodes();
+renderNodes(testNodes);
