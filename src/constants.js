06b673b99931a7486e605618c75364101f0e1125
zinaida <zinaida.j84@gmail.com>
2022-12-13 14:17:18 +0100

    tow constants added

--- src/constants.js
+++ src/constants.js
@@ -5,11 +5,29 @@ const ENTITY_EL = document.getElementById("game");
 const WALL_SOURCE = "./assets/images/wall.png";
 const PALLET_SOURCE = "./assets/images/pellet/pellet.png";
 const PALLET_POW_SOURCE = "../assets/images/pellet/power-pellet.png";
-const pmRightAni = ["./assets/images/pacman/pacman-0.png", "./assets/images/pacman/pacman-right/pacman-right-1.png", "./assets/images/pacman/pacman-right/pacman-right-2.png"];
-const pmLeftAni = ["./assets/images/pacman/pacman-0.png", "./assets/images/pacman/pacman-left/pacman-left-1.png", "./assets/images/pacman/pacman-left/pacman-left-2.png"];
-const pmUpAni = ["./assets/images/pacman/pacman-0.png", "./assets/images/pacman/pacman-up/pacman-up-1.png", "./assets/images/pacman/pacman-up/pacman-up-2.png"];
-const pmDownAni = ["./assets/images/pacman/pacman-0.png", "./assets/images/pacman/pacman-down/pacman-down-1.png", "./assets/images/pacman/pacman-down/pacman-down-2.png"]
+const pmRightAni = [
+  "./assets/images/pacman/pacman-0.png",
+  "./assets/images/pacman/pacman-right/pacman-right-1.png",
+  "./assets/images/pacman/pacman-right/pacman-right-2.png",
+];
+const pmLeftAni = [
+  "./assets/images/pacman/pacman-0.png",
+  "./assets/images/pacman/pacman-left/pacman-left-1.png",
+  "./assets/images/pacman/pacman-left/pacman-left-2.png",
+];
+const pmUpAni = [
+  "./assets/images/pacman/pacman-0.png",
+  "./assets/images/pacman/pacman-up/pacman-up-1.png",
+  "./assets/images/pacman/pacman-up/pacman-up-2.png",
+];
+const pmDownAni = [
+  "./assets/images/pacman/pacman-0.png",
+  "./assets/images/pacman/pacman-down/pacman-down-1.png",
+  "./assets/images/pacman/pacman-down/pacman-down-2.png",
+];
 const pmAniSpeed = 15;
+const nodeSymbols = ["X", "+", "x"];
+const pathSymbols = ["."];
 
 const LEVEL_PATTERN = {
   sizeX: 20,
