d3748a41191f6790cfadea0fc481b59f1cdf7c81
zinaida <zinaida.j84@gmail.com>
2022-12-13 17:17:56 +0100

    getStartPosition added to pmx, pmy

--- pacman.js
+++ pacman.js
@@ -1,100 +1,110 @@
-const SPRITE_SIZE = 40;
+const SPRITE_SIZE = 20;
 const SCREEN_END = 800;
 const SCREEN_START = 0;
 const VELOCITY = 5;
 const FPS = 60;
-const DT = 1000/FPS;
+const DT = 1000 / FPS;
 
 const pacman = document.querySelector("#pacman");
-const pacmanX = pacman.left
+const pacmanX = pacman.left;
 
 let chosenPmMoveDir = "STILL";
 let lastPmMove = "STILL";
-let pmX = 160;
-let pmY = 160;
+let pmX = getStartPosition(LEVEL_0).x;
+let pmY = getStartPosition(LEVEL_0).y;
 let frameForAnimation = 1;
 
 function main() {
-    let pmMoveDir = pacmanValidMove(pmX, pmY, testNodesVar, chosenPmMoveDir, lastPmMove)
-    lastPmMove = pmMoveDir;
+  let pmMoveDir = pacmanValidMove(
+    pmX,
+    pmY,
+    testNodesVar,
+    chosenPmMoveDir,
+    lastPmMove
+  );
+  lastPmMove = pmMoveDir;
 
-    if (pmMoveDir === "RIGHT") {
-        pacmanMovesRight();
-        // pacmanAnimation(pmRightAni);
-    } else if (pmMoveDir === "LEFT") {
-        pacmanMovesLeft();
-        // pacmanAnimation(pmLeftAni);
-    } else if (pmMoveDir === "UP") {
-        pacmanMovesUp();
-        // pacmanAnimation(pmUpAni);
-    } else if (pmMoveDir === "DOWN") {
-        pacmanMovesDown();
-        // pacmanAnimation(pmDownAni);
-    } else {
-        pacman.style.left = `${pmX}px`
-        pacman.style.top = `${pmY}px`
-        
-    }
-// pacmanAnimation();
+  if (pmMoveDir === "RIGHT") {
+    pacmanMovesRight();
+    // pacmanAnimation(pmRightAni);
+  } else if (pmMoveDir === "LEFT") {
+    pacmanMovesLeft();
+    // pacmanAnimation(pmLeftAni);
+  } else if (pmMoveDir === "UP") {
+    pacmanMovesUp();
+    // pacmanAnimation(pmUpAni);
+  } else if (pmMoveDir === "DOWN") {
+    pacmanMovesDown();
+    // pacmanAnimation(pmDownAni);
+  } else {
+    pacman.style.left = `${pmX}px`;
+    pacman.style.top = `${pmY}px`;
+  }
+  // pacmanAnimation();
 }
 
 addEventListener("keydown", (event) => {
-    if (event.key === "d" || event.key === "ArrowRight") {
-        chosenPmMoveDir = "RIGHT";
-    } else if (event.key === "a" || event.key === "ArrowLeft") {
-        chosenPmMoveDir = "LEFT";
-    } else if (event.key === "w" || event.key === "ArrowUp") {
-        chosenPmMoveDir = "UP";
-    } else if (event.key === "s" || event.key === "ArrowDown") {
-        chosenPmMoveDir = "DOWN";
-    }
-})
+  if (event.key === "d" || event.key === "ArrowRight") {
+    chosenPmMoveDir = "RIGHT";
+  } else if (event.key === "a" || event.key === "ArrowLeft") {
+    chosenPmMoveDir = "LEFT";
+  } else if (event.key === "w" || event.key === "ArrowUp") {
+    chosenPmMoveDir = "UP";
+  } else if (event.key === "s" || event.key === "ArrowDown") {
+    chosenPmMoveDir = "DOWN";
+  }
+});
 
 function pacmanValidMove(pmX, pmY, testNodesVar, chosenPmMoveDir, lastPmMove) {
-    let nodeHit = false;
-    let neighborGiven = false;
-    for (let node of testNodesVar) {
-        if (node.position[0] === pmX && node.position[1] === pmY) {
-            nodeHit = true;
-            if (node.neighbors[chosenPmMoveDir] !== null) {
-                neighborGiven = true;
-            }
-        }
+  let nodeHit = false;
+  let neighborGiven = false;
+  for (let node of testNodesVar) {
+    if (node.position[0] === pmX && node.position[1] === pmY) {
+      nodeHit = true;
+      if (node.neighbors[chosenPmMoveDir] !== null) {
+        neighborGiven = true;
+      }
     }
-    if (nodeHit === false) {
-        return chosenPmMoveDir === "RIGHT" && lastPmMove === "LEFT" || chosenPmMoveDir === "LEFT" && lastPmMove === "RIGHT" || chosenPmMoveDir === "UP" && lastPmMove === "DOWN" || chosenPmMoveDir === "DOWN" && lastPmMove === "UP" ? chosenPmMoveDir : lastPmMove;
-    } else if (nodeHit === true) {
-        return neighborGiven === false ? "STILL" : chosenPmMoveDir;
-    }
+  }
+  if (nodeHit === false) {
+    return (chosenPmMoveDir === "RIGHT" && lastPmMove === "LEFT") ||
+      (chosenPmMoveDir === "LEFT" && lastPmMove === "RIGHT") ||
+      (chosenPmMoveDir === "UP" && lastPmMove === "DOWN") ||
+      (chosenPmMoveDir === "DOWN" && lastPmMove === "UP")
+      ? chosenPmMoveDir
+      : lastPmMove;
+  } else if (nodeHit === true) {
+    return neighborGiven === false ? "STILL" : chosenPmMoveDir;
+  }
 }
 
 function pacmanMovesRight() {
-    pmX += VELOCITY;
-    if (pmX === SCREEN_END) {
-        pmX = SCREEN_START;
-    }
-    pacman.style.left = `${pmX}px`;
+  pmX += VELOCITY;
+  if (pmX === SCREEN_END) {
+    pmX = SCREEN_START;
+  }
+  pacman.style.left = `${pmX}px`;
 }
 function pacmanMovesLeft() {
-    pmX -= VELOCITY;
-    if (pmX === SCREEN_START - SPRITE_SIZE) {
-        pmX = SCREEN_END - SPRITE_SIZE;
-    }
-    pacman.style.left = `${pmX}px`;
+  pmX -= VELOCITY;
+  if (pmX === SCREEN_START - SPRITE_SIZE) {
+    pmX = SCREEN_END - SPRITE_SIZE;
+  }
+  pacman.style.left = `${pmX}px`;
 }
 function pacmanMovesUp() {
-    pmY -= VELOCITY;
-    if (pmY === SCREEN_START - SPRITE_SIZE) {
-        pmY = SCREEN_END - SPRITE_SIZE;
-    }
-    pacman.style.top = `${pmY}px` ;
+  pmY -= VELOCITY;
+  if (pmY === SCREEN_START - SPRITE_SIZE) {
+    pmY = SCREEN_END - SPRITE_SIZE;
+  }
+  pacman.style.top = `${pmY}px`;
 }
 function pacmanMovesDown() {
-    pmY += VELOCITY;
-    if (pmY === SCREEN_END) {
-        pmY = SCREEN_START;
-    }
-    pacman.style.top = `${pmY}px`;
+  pmY += VELOCITY;
+  if (pmY === SCREEN_END) {
+    pmY = SCREEN_START;
+  }
+  pacman.style.top = `${pmY}px`;
 }
 
 // function pacmanAnimation(arr) {
@@ -112,5 +122,6 @@ function pacmanMovesDown() {
 // for (let node of testNodesVar) {
 //     console.log(node.position[0], node.position[1], node.neighbors[])
 // }
-setInterval(main, DT)
-console.log(testNodesVar[4].position[0], testNodesVar[4].position[1])
+
+setInterval(main, DT);
+console.log(testNodesVar[4].position[0], testNodesVar[4].position[1]);
