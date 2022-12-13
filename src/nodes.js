f30363f03e472bec215765ec7cc406938705e6e9
zinaida <zinaida.j84@gmail.com>
2022-12-13 14:15:35 +0100

    added consolePrint function

--- src/nodes.js
+++ src/nodes.js
@@ -11,6 +11,19 @@ class Node {
   get getY() {
     return this.y;
   }
+  get nodeLeft() {
+    return this.neighbors.LEFT;
+  }
+  get nodeRight() {
+    return this.neighbors.RIGHT;
+  }
+  get nodeUp() {
+    return this.neighbors.UP;
+  }
+  get nodeDown() {
+    return this.neighbors.DOWN;
+  }
+
   set right(newRight) {
     this.neighbors.RIGHT = newRight;
   }
@@ -24,10 +37,9 @@ class Node {
     this.neighbors.DOWN = newDown;
   }
 }
-// test file
-//8X8
-const nodeSymbols = ["X", "+", "x"];
-const pathSymbols = ["."];
+
+//############## MAZE-STRING FUNCTIONS ##############################
+
 const mazeTest = {
   sizeX: 8,
   sizeY: 8,
@@ -44,10 +56,30 @@ const mazeTest = {
 
 function transposeMaze(level) {
   let tMaze = "";
-  // for (let x = 0; x < level.sizeY)
+  let count = 0;
+  for (let y = 0; y < level.sizeY; y++) {
+    for (let x = 0; x < level.sizeX; x++) {
+      let position = x * level.sizeX + y;
+      tMaze += level.pattern[position];
+    }
+    // tMaze += "\n";
+  }
+  return { sizeX: level.sizeY, sizeY: level.sizeX, pattern: tMaze };
 }
-transposeMaze(mazeTest);
 
+function convertMazeTo2D(level) {
+  let newMaze = [];
+  let row = [];
+  for (let i = 0; i < level.pattern.length; i++) {
+    row.push(level.pattern[i]);
+    if (row.length === level.sizeX) {
+      newMaze.push(row);
+      row = [];
+    }
+  }
+  return newMaze;
+}
+//####################################################################
 function constructKey(x, y, dx = RASTER_SIZE, dy = RASTER_SIZE) {
   return { x: x * dx, y: y * dy };
 }
@@ -68,18 +100,12 @@ function createNodeTable(level) {
   return nodeList;
 }
 
-// TEST
-let nodes = createNodeTable(mazeTest);
-// console.log(nodes);
-
-function getNodeByCoord(x, y, nodes) {
-  for (const node of nodes) {
+function getNodeByCoord(x, y, nodeList) {
+  for (const node of nodeList) {
     if (node.getX === x && node.getY === y) return node;
   }
   return null;
 }
-// TEST
-// console.log(getNodeByCoord(100, 25, nodes));
 
 function connectHorizontally(level, nodeList) {
   let count = 0;
@@ -103,14 +129,94 @@ function connectHorizontally(level, nodeList) {
   }
   return;
 }
-//TEST connect Horizontally
-connectHorizontally(mazeTest, nodes);
-// console.log(nodes);
 
-function connectVertically() {
-  return;
+function connectVertically(level, nodeList) {
+  let tMaze = transposeMaze(level);
+  let count = 0;
+  for (let y = 0; y < tMaze.sizeY; y++) {
+    let key = null;
+    for (let x = 0; x < tMaze.sizeX; x++) {
+      if (nodeSymbols.includes(tMaze.pattern[count])) {
+        if (key === null) {
+          key = constructKey(y, x);
+        } else {
+          otherkey = constructKey(y, x);
+          let firstNode = getNodeByCoord(key.x, key.y, nodeList);
+          let secondNode = getNodeByCoord(otherkey.x, otherkey.y, nodeList);
+          firstNode.down = secondNode;
+          secondNode.up = firstNode;
+          key = otherkey;
+        }
+      }
+      count += 1;
+    }
+  }
 }
 
+// check for bug
+function removeRedundantConnections(level, nodeList) {
+  let maze = convertMazeTo2D(level);
+  for (let y = 1; y < maze.length - 1; y++) {
+    for (let x = 1; x < maze[y].length - 1; x++) {
+      if (nodeSymbols.includes(maze[y][x])) {
+        let key = constructKey(x, y);
+        let tempNode = getNodeByCoord(key.x, key.y, nodeList);
+
+        if (!pathSymbols.includes(maze[y][x + 1])) {
+          tempNode.right = null;
+        }
+        if (!pathSymbols.includes(maze[y][x - 1])) {
+          tempNode.left = null;
+        }
+        if (!pathSymbols.includes(maze[y + 1][x])) {
+          tempNode.up = null;
+        }
+        if (!pathSymbols.includes(maze[y - 1][x])) {
+          tempNode.down = null;
+        }
+      }
+    }
+  }
+}
+
+function consolePrintNodes(level, nodeList) {
+  // console.log(level.sizeX, level.sizeY);
+  let nodeArray = [];
+  for (let i = 0; i < level.sizeY; i++) {
+    let row = Array(level.sizeX).fill("  ");
+    nodeArray.push(row);
+  }
+  // console.log(nodeArray);
+  let maze = convertMazeTo2D(level);
+
+  for (let y = 1; y < maze.length - 1; y++) {
+    for (let x = 1; x < maze[y].length - 1; x++) {
+      if (nodeSymbols.includes(maze[y][x])) {
+        nodeArray[y][x] = "X ";
+        let key = constructKey(x, y);
+        let tempNode = getNodeByCoord(key.x, key.y, nodeList);
+        console.log(tempNode);
+
+        if (tempNode.nodeRight !== null) {
+          nodeArray[y][x + 1] = "- ";
+        }
+        if (tempNode.nodeLeft !== null) {
+          nodeArray[y][x - 1] = " -";
+        }
+        if (tempNode.nodeUp !== null) {
+          nodeArray[y - 1][x] = "| ";
+        }
+        if (tempNode.nodeDown !== null) {
+          nodeArray[y + 1][x] = "| ";
+        }
+      }
+    }
+  }
+
+  console.log(nodeArray.map((e) => e.join("")).join("\n"));
+}
+
+//########### TEST FUNCTIONS ##########################
 function testNodes() {
   let nodeA = new Node(80, 80);
   let nodeB = new Node(160, 80);
@@ -146,3 +252,13 @@ function renderNodes(nodesList) {
 
 let testNodesVar = testNodes();
 renderNodes(testNodesVar);
+//#####################################################
+
+function createNodeChain(level) {
+  let nodes = createNodeTable(level);
+  connectHorizontally(level, nodes);
+  connectVertically(level, nodes);
+  // removeRedundantConnections(level, nodes);
+  consolePrintNodes(mazeTest, nodes);
+}
+createNodeChain(mazeTest);
