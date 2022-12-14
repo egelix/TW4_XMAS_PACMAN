e5a783f2c9539a6c05b9c1304b8c8737952375ff
zinaida <zinaida.j84@gmail.com>
2022-12-14 11:15:32 +0100

    add createMaze, createNodeChain, renderNodes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+/* 
+const PACMAN = createSprite("pacman");
+const BLINKY = createSprite("blinky");
+const PINKY = createSprite("pinky");
+const INKY = createSprite("inky");
+const CLYDE = createSprite("clyde");
+
+let testNodesVar = createNodeChain(LEVEL_0);
+
+*/
+const maze = createMaze(LEVEL_0);
+let testNodesVar = createNodeChain(LEVEL_0);
+renderNodes(testNodesVar);
+
 function update() {}
+
+/*
+setInterval(update, DT); 
+ */
